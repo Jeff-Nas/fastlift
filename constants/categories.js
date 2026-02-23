9dd304d55013da1a88879b980023d577f241f512
Jeferson Nascimento <jeferson.ifce@gmail.com>
2026-02-23 14:33:11 -0300

    Arquivo categories criado - filtro de máquinas

---
+++ constants/categories.js
@@ -0,0 +1,42 @@
+// lib/categories.js
+
+export const CATEGORIES = [
+  {
+    slug: "scissor-lift",
+    label: "Tesoura Elétrica",
+    image: "/images/scissor-lift-mini.png",
+    searchTerms: ["electric", "scissor-lift"],
+  },
+  {
+    slug: "scissor-lift",
+    label: "Tesoura Díesel",
+    image: "/images/scissors-lift.png",
+    searchTerms: ["diesel", "scissor-lift"],
+  },
+  {
+    slug: "mast-lift",
+    label: "Elevador-Mastro",
+    image: "/images/mastro-vertical.png",
+    searchTerms: ["electric", "mast-lift"],
+  },
+  {
+    slug: "boom-lift",
+    label: "Boom Elétrico",
+    image: "/images/electric-boom.png",
+    searchTerms: ["electric", "boom-lift"],
+  },
+  {
+    slug: "boom-lift",
+    label: "Boom Díesel",
+    image: "/images/boom-lift.png",
+    searchTerms: ["diesel", "boom-lift"],
+  },
+  {
+    slug: "big-boom",
+    label: "Big Boom",
+    image: "/images/big-boom.png",
+    searchTerms: ["diesel", "big-boom"],
+  },
+];
+
+export const MANUFACTURERS = ["jlg", "genie", "skyjack", "haulotte"];
