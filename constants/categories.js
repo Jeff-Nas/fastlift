58a1a4f7493e9a52c0e260c9cb1c6e8f819a0b31
Jeferson Nascimento <jeferson.ifce@gmail.com>
2026-02-24 18:21:03 -0300

    Corrigindo camimho das imagens em CATEGORIES

--- constants/categories.js
+++ constants/categories.js
@@ -4,37 +4,37 @@ export const CATEGORIES = [
   {
     slug: "scissor-lift",
     label: "Tesoura Elétrica",
-    image: "/images/scissor-lift-mini.png",
+    image: "/images/vectors/scissor-lift-mini.png",
     searchTerms: ["electric", "scissor-lift"],
   },
   {
     slug: "scissor-lift",
     label: "Tesoura Díesel",
-    image: "/images/scissors-lift.png",
+    image: "/images/vectors/scissors-lift.png",
     searchTerms: ["diesel", "scissor-lift"],
   },
   {
     slug: "mast-lift",
     label: "Elevador-Mastro",
-    image: "/images/mastro-vertical.png",
+    image: "/images/vectors/mastro-vertical.png",
     searchTerms: ["electric", "mast-lift"],
   },
   {
     slug: "boom-lift",
     label: "Boom Elétrico",
-    image: "/images/electric-boom.png",
+    image: "/images/vectors/electric-boom.png",
     searchTerms: ["electric", "boom-lift"],
   },
   {
     slug: "boom-lift",
     label: "Boom Díesel",
-    image: "/images/boom-lift.png",
+    image: "/images/vectors/boom-lift.png",
     searchTerms: ["diesel", "boom-lift"],
   },
   {
     slug: "big-boom",
     label: "Big Boom",
-    image: "/images/big-boom.png",
+    image: "/images/vectors/big-boom.png",
     searchTerms: ["diesel", "big-boom"],
   },
 ];
