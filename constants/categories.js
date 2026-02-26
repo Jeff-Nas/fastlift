efcba1581c97ccc6b2e8d70fbe85404b6f42a5d2
Jeferson Nascimento <jeferson.ifce@gmail.com>
2026-02-26 17:34:53 -0300

    Modificando slug para exibir o path do JSON

--- constants/categories.js
+++ constants/categories.js
@@ -2,40 +2,34 @@
 
 export const CATEGORIES = [
   {
-    slug: "scissor-lift",
+    slug: "electric/scissor-lift",
     label: "Tesoura Elétrica",
     image: "/images/vectors/scissor-lift-mini.png",
-    searchTerms: ["electric", "scissor-lift"],
   },
   {
-    slug: "scissor-lift",
+    slug: "diesel/scissor-lift",
     label: "Tesoura Díesel",
     image: "/images/vectors/scissors-lift.png",
-    searchTerms: ["diesel", "scissor-lift"],
   },
   {
-    slug: "mast-lift",
+    slug: "electric/mast-lift",
     label: "Elevador-Mastro",
     image: "/images/vectors/mastro-vertical.png",
-    searchTerms: ["electric", "mast-lift"],
   },
   {
-    slug: "boom-lift",
+    slug: "electric/boom-lift",
     label: "Boom Elétrico",
     image: "/images/vectors/electric-boom.png",
-    searchTerms: ["electric", "boom-lift"],
   },
   {
-    slug: "boom-lift",
+    slug: "diesel/boom-lift",
     label: "Boom Díesel",
     image: "/images/vectors/boom-lift.png",
-    searchTerms: ["diesel", "boom-lift"],
   },
   {
-    slug: "big-boom",
+    slug: "diesel/big-boom",
     label: "Big Boom",
     image: "/images/vectors/big-boom.png",
-    searchTerms: ["diesel", "big-boom"],
   },
 ];
 
