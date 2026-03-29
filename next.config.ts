e99dad976e68534e39a42abbf0b6de7c3cdc0f4d
Jeferson Nascimento <jeferson.ifce@gmail.com>
2026-03-29 14:20:39 -0300

    Autorizando  link externo para Image

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "imagens.fastlift.com.br",
+        port: "",
+        pathname: "/**", // Permite todas as pastas dentro do domínio
+      },
+    ],
+  },
 };
 
 export default nextConfig;
