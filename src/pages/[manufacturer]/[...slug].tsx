3fa77b2208dccbdceab3f9de611e7f494876a700
Jeferson Nascimento <jeferson.ifce@gmail.com>
2026-03-31 09:34:00 -0300

    Aplicando seleção do primeiro manual com useEffect

--- src/pages/[manufacturer]/[...slug].tsx
+++ src/pages/[manufacturer]/[...slug].tsx
@@ -1,63 +1,81 @@
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import manualsData from "../../../data/lista_manuais.json";
 import imagesData from "../../../data/lista_imagens.json";
 import { mapManualToFrontEnd, mapImageToFront } from "@/lib/manualMappers";
 import { ModelCard } from "@/components/ui/modelCard";
 import Image from "next/image";
 import { MANUFACTURERS } from "../../../constants/categories";
+import { RotateCw } from "lucide-react";
 export default function ManualsPage() {
-  const [selectedModel, setSelectedModel] = useState<string | null>(null); //selecionar primeiro elemento do array
+  const [selectedMachine, setSelectedMachine] = useState<string | null>(null); //selecionar primeiro elemento do array
 
   const router = useRouter();
-  // if (!router.isReady) return null; validação futura com carregamento
+
+  if (!router.isReady) {
+    return (
+      <div className="bg-slate-50 h-dvh mx-auto w-full flex flex-col items-center justify-center gap-2">
+        <RotateCw size={40} className="animate-spin text-orange-400" />
+        <p className="font-light">Carregando...</p>
+      </div>
+    );
+  }
+
   const { manufacturer, slug } = router.query;
   //path to filter manual array
   const modelPath = Array.isArray(slug) ? slug.join("/") : "";
   const fullPath = `${manufacturer}/${modelPath}`;
-  const machineModels = manualsData.filter((manual) =>
-    manual.caminho_r2.includes(fullPath),
-  );
   const manualExist = imagesData.some((item) =>
     item.caminho_r2.toLowerCase().includes(fullPath.toLowerCase()),
   );
 
   //path to filter image array
-  const modelIMages = imagesData.filter((model) =>
-    model.caminho_r2.includes(fullPath),
+  const machineImages = imagesData.filter((machine) =>
+    machine.caminho_r2.includes(fullPath),
+  );
+  const machineManuals = manualsData.filter((manual) =>
+    manual.caminho_r2.includes(fullPath),
   );
   //Adapting props for camelCase
 
-  const manualModelAdapted = machineModels.map((model) =>
+  const machineManualsAdapted = machineManuals.map((model) =>
     mapManualToFrontEnd(model),
   );
-  const selectedManuals = manualModelAdapted.filter((manual) =>
-    manual.caminhoR2.toLowerCase().includes(selectedModel),
-  );
-  console.log(selectedManuals);
-  console.log(selectedModel);
 
-  const imageModelAdapted = modelIMages.map((model) => mapImageToFront(model));
+  const machineImagesAdapted = machineImages.map((model) =>
+    mapImageToFront(model),
+  );
+
+  const selectedManuals = machineManualsAdapted.filter((manual) =>
+    manual.caminhoR2.toLowerCase().includes(selectedMachine),
+  );
 
   const manufacturerData = MANUFACTURERS.find(
     (brand) => brand.label === manufacturer,
   );
 
-  const handleSelectModel = (filter: string) => {
-    setSelectedModel(filter);
+  useEffect(() => {
+    if (machineImagesAdapted.length > 0 && !selectedMachine) {
+      setSelectedMachine(machineImagesAdapted[0].filtro);
+    }
+  }, [machineImagesAdapted, selectedMachine]);
+
+  const handleSelectMachine = (filter: string) => {
+    setSelectedMachine(filter);
     console.log(`This is the filter: ${filter}`);
+    console.log(selectedManuals);
   };
 
   return (
     <div className="bg-slate-50 h-dvh p-2">
       {manualExist ? (
         <div className="w-full grid grid-cols-2 gap-3 mx-auto md:grid-cols-4 lg:grid-cols-7 lg:gap-1 lg:mx-8">
-          {imageModelAdapted.map((model) => (
+          {machineImagesAdapted.map((model) => (
             <ModelCard
               item={model}
               key={model.caminhoR2}
-              onClick={() => handleSelectModel(model.filtro)}
-              active={selectedModel === model.filtro}
+              onClick={() => handleSelectMachine(model.filtro)}
+              active={selectedMachine === model.filtro}
             />
           ))}
         </div>
@@ -69,7 +87,7 @@ export default function ManualsPage() {
             informações.
           </p>
           <a
-            href={manufacturerData?.site}
+            href={manufacturerData?.site || "#"}
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block p-2 bg-orange-500 rounded-sm text-white hover:bg-orange-500 transition-colors text-center"
