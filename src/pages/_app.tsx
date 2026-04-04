360aa0526de6c08193684b5425f92b6fc864b623
Jeferson Nascimento <jeferson.ifce@gmail.com>
2026-04-04 17:02:07 -0300

    Tag do Google Analytics adicionada

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Script Principal do Google */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-DLE0CW8HRQ"
+        strategy="afterInteractive"
+      />
+      {/* Inicialização do Analytics */}
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-DLE0CW8HRQ');
+        `}
+      </Script>
+      <Component {...pageProps} />;
+    </>
+  );
 }
