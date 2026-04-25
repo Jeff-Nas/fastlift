689c204e0ce670e4ca4f4055f33f05d80704072c
Jeferson Nascimento <jeferson.ifce@gmail.com>
2026-04-25 11:59:27 -0300

    OpenGraph definido no Layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,13 +3,14 @@ import { ReactNode } from "react";
 import Header from "./header";
 import Footer from "@/components/footer";
 import Head from "next/head";
-
+import OpenGraph from "./openGraph";
 interface LayoutProps {
   children: ReactNode; //React.ReactNode
 }
 export default function Layout({ children }: LayoutProps) {
   return (
     <>
+      <OpenGraph />
       <Head>
         <title>FastLift</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
